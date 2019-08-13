fad20cdbda364d68417866c842121660fd64cb58
Ryan Quellhorst <rquellhorst@hmbnet.com>
2019-08-13 14:41:30 -0400

    fixed the header icon

--- src/components/IndexNavbar.vue
+++ src/components/IndexNavbar.vue
@@ -1,14 +1,19 @@
 <template>
   <v-toolbar flat app dense dark color="primary">
-    <v-img :src="require('@/assets/images/header.png')" :aspect-ratio="1" max-width="25px" contain></v-img>
+    <v-avatar size="25">
+      <img :src="require('@/assets/images/header.png')" />
+    </v-avatar>
+
     <div class="pl-3">QA or the Highway</div>
     <v-spacer></v-spacer>
     <v-btn flat @click="favorites.filterall = !favorites.filterall">
       <div v-if="favorites.filterall" class="text-capitalize">
-        Filtered<v-icon right small color="amber">fas fa-star</v-icon>
+        Filtered
+        <v-icon right small color="amber">fas fa-star</v-icon>
       </div>
       <div v-else class="text-capitalize">
-        Filter<v-icon right small>far fa-star</v-icon>
+        Filter
+        <v-icon right small>far fa-star</v-icon>
       </div>
     </v-btn>
   </v-toolbar>
