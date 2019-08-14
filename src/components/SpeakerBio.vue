be59b1595bcc4cbf5fc94aa81b1919960cbb6d19
Ryan Quellhorst <rquellhorst@hmbnet.com>
2019-08-14 08:44:06 -0400

    fixed styling in speaker bio

--- src/components/SpeakerBio.vue
+++ src/components/SpeakerBio.vue
@@ -1,30 +1,25 @@
 <template>
   <v-layout ma-0 pa-0 row wrap>
-    <v-flex xs12 sm4 pa-3 align-self-center>
-      <v-img
-        :src="require('@/assets/images/generic-profile.png')"
-        :aspect-ratio="1"
-        max-height="300px"
-        contain
-      ></v-img>
+    <v-flex xs12 sm4 pa-3 justify-center align-center layout>
+      <v-avatar size="300">
+        <img :src="require('@/assets/images/generic-profile.png')" />
+      </v-avatar>
     </v-flex>
     <v-flex xs12 sm8 pa-3>
-      <v-layout row align-center ma-0 pa-0 wrap>
-        <v-flex>
-          <div class="headline font-weight-bold">{{ speaker }}</div>
-        </v-flex>
-        <v-flex>
-          <v-btn flat icon color="primary" :href="linkUrl">
-            <v-icon>fab fa-linkedin</v-icon>
-          </v-btn>
-          <v-btn flat icon color="primary" :href="twitUrl">
-            <v-icon>fab fa-twitter</v-icon>
-          </v-btn>
-          <v-btn flat icon color="primary" :href="webUrl">
-            <v-icon>fas fa-laptop</v-icon>
-          </v-btn>
-        </v-flex>
+      <v-layout align-center>
+        <div class="headline font-weight-bold">{{ speaker }}</div>
+        <v-spacer />
+        <v-btn flat icon color="primary" :href="linkUrl">
+          <v-icon>fab fa-linkedin</v-icon>
+        </v-btn>
+        <v-btn flat icon color="primary" :href="twitUrl">
+          <v-icon>fab fa-twitter</v-icon>
+        </v-btn>
+        <v-btn flat icon color="primary" :href="webUrl">
+          <v-icon>fas fa-laptop</v-icon>
+        </v-btn>
       </v-layout>
+
       <div class="subheading">{{ bio }}</div>
     </v-flex>
   </v-layout>
@@ -32,6 +27,6 @@
 
 <script>
 export default {
-    props: ["img", "speaker", "bio", "linkUrl", "twitUrl", "webUrl"]
-}
+  props: ["img", "speaker", "bio", "linkUrl", "twitUrl", "webUrl"]
+};
 </script>
