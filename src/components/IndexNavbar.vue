533bd6cc2de94a43bc4832d57f6f2d801c089fa0
Ryan Quellhorst <rquellhorst@hmbnet.com>
2019-08-28 09:47:32 -0400

    added logo font to index header

--- src/components/IndexNavbar.vue
+++ src/components/IndexNavbar.vue
@@ -3,7 +3,7 @@
     <v-avatar size="25">
       <img :src="require('@/assets/images/header.png')" alt="QA or the Highway logo"/>
     </v-avatar>
-    <div class="pl-3">QA or the Highway</div>
+    <div class="pl-3 branded_font">QA or the Highway</div>
     <v-spacer></v-spacer>
     <v-btn flat @click="favorites.filterall = !favorites.filterall">
       <div v-if="favorites.filterall" class="text-capitalize">
@@ -25,4 +25,11 @@ export default {
   props: ["favorites"]
 };
 </script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:500&display=swap');
+.branded_font {
+  font-family: 'Montserrat', 'Roboto', sans-serif; 
+}
+</style>
 
