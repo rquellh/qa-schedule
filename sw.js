a9cbc8ca203a177f31eda87a8c4e6fc1c64d135f
Ryan Quellhorst <ryanquellhorst@Admins-MBP.deeplens>
2020-02-05 14:23:27 -0500

    Updates

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
 
-workbox.clientsClaim();
+workbox.core.skipWaiting();
+workbox.core.clientsClaim();
 
 // cache name
 workbox.core.setCacheNameDetails({
